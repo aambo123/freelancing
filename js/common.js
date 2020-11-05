c0405923c7080f6b39e2374669051c3e9155877f
aambo123 <45193265+aambo123@users.noreply.github.com>
2020-11-05 13:55:36 +0800

    add pages
    add pages

--- js/common.js
+++ js/common.js
@@ -1,55 +1,149 @@
-fetch("/html/template/header.html")
-    .then(response => {
-        return response.text()
-    })
-    .then(data => {
-        document.querySelector("header").innerHTML = data;
-    });
+var isIE = !!window.MSInputMethodContext && !!document.documentMode;
 
-fetch("/html/template/footer.html")
-    .then(response => {
-        return response.text()
-    })
-    .then(data => {
-        document.querySelector("footer").innerHTML = data;
-    });/*  */
+if (isIE) {
+    // Create Promise polyfill script tag
+    var promiseScript = document.createElement("script");
+    promiseScript.type = "text/javascript";
+    promiseScript.src =
+        "https://cdn.jsdelivr.net/npm/promise-polyfill@8.1.3/dist/polyfill.min.js";
 
-fetch("/html/template/head.html")
-    .then(response => {
-        return response.text()
-    })
-    .then(data => {
-        document.querySelector("head").innerHTML = data;
-    });/*  */
+    // Create Fetch polyfill script tag
+    var fetchScript = document.createElement("script");
+    fetchScript.type = "text/javascript";
+    fetchScript.src =
+        "https://cdn.jsdelivr.net/npm/whatwg-fetch@3.4.0/dist/fetch.umd.min.js";
+
+    // Add polyfills to head element
+    document.head.appendChild(promiseScript);
+    document.head.appendChild(fetchScript);
+
+    // Wait for the polyfills to load and run the function. 
+    // We could have done this differently, 
+    // but I've found it to work well for my use-cases
+    setTimeout(function () {
+        window
+            .fetch("/html/template/header.html")
+            .then(function (response) {
+                return response.text()
+            })
+            .then(function (data) {
+                checkHeader(data);
+            });
+
+        window
+            .fetch("/html/template/footer.html")
+            .then(function (response) {
+                return response.text()
+            })
+            .then(function (data) {
+                document.querySelector("footer").innerHTML = data;
+            });/*  */
+        window
+            .fetch("/html/template/head.html")
+            .then(function (response) {
+                return response.text()
+            })
+            .then(function (data) {
+                document.querySelector("head").innerHTML = data;
+            });/*  */
 
 
-fetch("/html/template/side_nav.html")
-    .then(response => {
-        return response.text()
-    })
-    .then(data => {
+        window
+            .fetch("/html/template/side_nav.html")
+            .then(function (response) {
+                return response.text()
+            })
+            .then(function (data) {
+                
+                activeNavs(data)
 
+            });/*  */
+
+    }, 1000);
+} else {
+    // If fetch is supported, just run the fetch function
+
+    fetch("/html/template/header.html")
+        .then(function (response) {
+            return response.text()
+        })
+        .then(function (data) {
+            
+            checkHeader(data)
+        });
+
+
+    fetch("/html/template/footer.html")
+        .then(function (response) {
+            return response.text()
+        })
+        .then(function (data) {
+            document.querySelector("footer").innerHTML = data;
+        });/*  */
+
+    fetch("/html/template/head.html")
+        .then(function (response) {
+            return response.text()
+        })
+        .then(function (data) {
+            document.querySelector("head").innerHTML = data;
+        });/*  */
+
+
+
+    fetch("/html/template/side_nav.html")
+        .then(function (response) {
+            return response.text()
+        })
+        .then(function (data) {
+    
+            activeNavs(data);
+            
+        });/*  */
+    }
+    
+    
+    function activeNavs(data){
         var html = $.parseHTML(data);
         var paths = location.pathname.split('/');
         var pathCount = paths.length;
         var lastFolder = paths[pathCount - 2];
         var fileName = paths[pathCount - 1];
         fileName = fileName.split('.').slice(0, -1).join('.')
-
+    
         var sMenu = $(html).filter('.side_nav[data-suffix=' + lastFolder + ']');
         var sideMenus = $(sMenu).find('.side_menu li a');
-
+    
         $(sideMenus).each(function (index, element) {
             var hrf = $(element).attr('href');
-            if (hrf && hrf.indexOf(fileName) > -1) {
-                $(element).addClass('active')
-                $(element).closest('dl').prev('a').addClass('active')
+            var searchValue = new URLSearchParams(location.search).get('search');
+            if(location.search != ""){
+                if(searchValue != null && $(element).data('search') == searchValue){
+                    $(element).addClass('active')
+                    $(element).closest('dl').prev('a').addClass('active')
+                }
+            }else{
+                if (hrf && hrf.indexOf(fileName) > -1) {
+                    $(element).addClass('active')
+                    $(element).closest('dl').prev('a').addClass('active')
+                }
             }
         });
+    
+        $(sMenu).prependTo('aside');
 
-        $(sMenu).appendTo('aside');
+}
 
-    });/*  */
+function checkHeader(data){
+    $(data).prependTo('header');
+    if(sessionStorage.getItem('logged_in') == "true"){
+        $('#notLoggedIn').remove()
+        $('#notLoggedInMob').remove()
+    }else{
+        $('#LoggedIn').remove()
+        $('#LoggedInMob').remove()
+    }
+}
 
 function menu(obj, e) {
     e.preventDefault();
@@ -186,3 +280,102 @@ function addComma(n) {
     return n;
 }
 
+
+function openLayer(obj) {
+    event.preventDefault();
+    var href = $(obj).data('href') ? $(obj).data('href') : '';
+
+    $('<div/>', {
+        id: 'modal',
+        "class": 'popup_wrapper'
+    }).appendTo('body');
+
+    $('#modal').waitMe({
+        effect: 'win8',
+        text: '',
+        bg: '',
+        color: 'white',
+        textPos: 'vertical',
+    })
+
+    if (href == '') {
+        alert('no link')
+        return false;
+    }
+    setTimeout(function () {
+        $.ajax({
+            type: "get",
+            url: href,
+            success: function (response) {
+                $('#modal').waitMe("hide")
+                $(response).appendTo('#modal');
+            }
+        });
+    }, 1000);
+
+}
+
+function closeLayer(){
+    event.preventDefault();
+    $('#modal').hide()
+    $('#modal').remove()
+}
+
+
+class user {
+    constructor(id,pass,type){
+        this.id = id;
+        this.pass = pass;
+        this.type = type;
+    }
+    checkId(id,pass){
+        return this.id == id && this.pass == pass;
+    }
+}
+
+let users = [
+    new user('user','123','freelancer'),    
+    new user('client','123','client')
+]
+
+//var loggedIn = '<a href="/html/login.html" class="btn btn_sm white">로그아웃</a><a href="/html/mypage.html" class="btn btn_sm black">마이페이지</a>'
+
+
+function checkLogin(){
+    
+    var id = document.getElementById('user_id').value;
+    var pass = document.getElementById("user_pass").value;
+    var userExist = false;
+    for (i = 0; i < users.length; i++) {
+        if(users[i].id == id){
+            var type = users[i].type;
+            if(users[i].checkId(id,pass)){
+                userExist = true;
+                doLogin(type,id);
+            }
+            break;
+        }
+    }
+    if(!userExist){
+        alert('없는 아이디입니다.')
+    }
+}
+
+function doLogin(type,id){
+    sessionStorage.setItem('logged_in', true);
+    sessionStorage.setItem('user_id', id);
+    sessionStorage.setItem('user_type', type);
+    location.href = "/html/main.html"
+}
+
+function doLogout(){
+    sessionStorage.setItem('logged_in', false);
+    sessionStorage.setItem('user_id', "");
+    sessionStorage.setItem('user_type', "");
+    location.href = "/html/main.html"
+}
+function myPage(){
+    var user_type = sessionStorage.getItem('user_type');
+    var mypage = user_type == "client" ? "/html/mypage/client/pending_project.html":"/html/mypage/freelancer/myprofile.html"
+    location.href = mypage
+}
